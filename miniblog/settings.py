2ffcf3298ffe6534aaa167d791554bb9634e2376
Niteshupadhyay <niteshupa.com>
2022-04-07 08:01:48 +0530

    Add all file

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'blog.middlewares.UnderConstructionMiddleware'
+    # 'blog.middlewares.UnderConstructionMiddleware'
 ]
 
 ROOT_URLCONF = 'miniblog.urls'
